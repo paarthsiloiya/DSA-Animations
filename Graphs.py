d9c141656724e23460c67edf412d26a642b4b6e3
Paarth Siloiya <paaarthsiloiya@gmail.com>
2025-06-28 23:15:19 +0530

    Created a jupyter notebook for better understanding of Graphs, Improved the undirected graph animation by a lot.

--- Graphs.py
+++ Graphs.py
@@ -9,13 +9,13 @@ TEXTCOL = ManimColor.from_hex("#000000")
 SELCOL = ManimColor.from_hex("#7a5bae")
 SORTCOL = ManimColor.from_hex("#4a2a90")
 
-LAYOT_SCALE = 1.4
+LAYOT_SCALE = 2.4
 FSIZE = 40
 FONT = 'JetBrains Mono'
 
 SWAP_FONT_SIZE = 38         # For "Found!", "SORT!", etc.
-EXPLANATORY_FONT_SIZE = 40  # For step-by-step explanations
-POINTER_FONT_SIZE = 28      # For "Low", "High", "Mid", etc.
+EXPLANATORY_FONT_SIZE = 30  # For step-by-step explanations
+DEGREE_FONT_SIZE = 17      # For "Low", "High", "Mid", etc.
 
 NODE_COL = BASECOL
 EDGE_COL = SELCOL
@@ -41,71 +41,82 @@ class Node(VGroup):
 
 class UndirectedGraphs(Scene):
     def construct(self):
-        vertices = [0, 1, 2, 3, 4]
-        edges = [(0,1), (0,2), (2,1), (3,2), (4,2)]
+        vertices = [4, 3, 2, 0, 1]
+        edges = [(0, 2), (0, 1), (1, 3), (4, 1), (4, 2), (2, 3), (3, 4)]
+        degrees = {0: 2, 1: 3, 2: 3, 3: 3, 4: 3}
 
-        layout = {
-            0: [-2.6, -1, 0],
-            1: [-2.6, 2, 0],
-            2: [0, 2, 0],
-            3: [0, -1, 0],
-            4: [2, 0.5, 0],
-        }
-
-        for l in layout.values():
-            l[0] *= LAYOT_SCALE
-            l[1] *= LAYOT_SCALE
-
-        graph = Graph(vertices, edges, layout=layout,
+        graph = Graph(vertices, edges, layout='circular', layout_scale=LAYOT_SCALE,
                       vertex_mobjects={v : Node(v) for v in vertices},
                       edge_config={"stroke_color": EDGE_COL, "stroke_width": 6})
         
         graph.center()
 
-        # self.add(graph)
         nodes = list(graph.vertices.values())
+        graphVertices = graph.vertices
         for _ in range(len(vertices)):
             self.play(*[FadeIn(nodes[_])], run_time=0.3)
             self.wait(0.1)
 
         self.wait(1)
 
-        nodeSurr = DashedVMobject(SurroundingRectangle(nodes[-1], color=TEXTCOL, buff=0.1, corner_radius=0.6))
+        self.play(graphVertices[1].Select(), run_time=0.5)
+        nodeSurr = DashedVMobject(SurroundingRectangle(graphVertices[1], color=TEXTCOL, buff=0.15, corner_radius=0.6))
         nodeText = Text("Node", font=FONT, color=TEXTCOL, font_size=FSIZE).next_to(nodeSurr, UP, buff=0.1)
         self.play(Create(nodeSurr), run_time=0.5)
         self.wait(0.2)
         self.play(Write(nodeText), run_time=0.5)
         self.wait(1.5)
-        self.play(Uncreate(nodeSurr), Unwrite(nodeText), run_time=0.5, lag_ratio=0.1)
+        self.play(graphVertices[1].Clear() ,Uncreate(nodeSurr), Unwrite(nodeText), run_time=0.5, lag_ratio=0.1)
 
         self.wait(1.3)
 
         for i, item in enumerate(graph.edges.items()):
             points, edge = item
-            dir = rotate_vector(edge.get_unit_vector(), -PI/2)
-            pointText = Text(f"({points[0]}-{points[1]})", font=FONT, color=TEXTCOL, font_size=FSIZE).move_to(edge.get_center() + dir)
+            n1, n2 = edges[i]
+            self.play(graphVertices[n1].Select(), graphVertices[n2].Select(), run_time=0.5)
+            pointText = Text(f"{points[0]}-{points[1]}", font=FONT, color=TEXTCOL, font_size=EXPLANATORY_FONT_SIZE).next_to(edge.get_center(), direction=rotate_vector(edge.get_unit_vector(), PI / 2), buff=0.2)
             self.play(Create(edge), run_time=0.5)
             self.play(Write(pointText), run_time=0.2)
 
             if i == 0:
                 # self.play(edge.animate.set_stroke(color=TEXTCOL), run_time=0.3)
                 self.wait(0.1)
-                nodeSurr = DashedVMobject(SurroundingRectangle(edge, color=TEXTCOL, buff=0.1, corner_radius=0.1), num_dashes=30).scale([1, 0.8, 1])
-                nodeText = Text("Edge", font=FONT, color=TEXTCOL, font_size=FSIZE).next_to(nodeSurr, LEFT, buff=0.1)
+                nodeSurr = DashedVMobject(SurroundingRectangle(edge, color=TEXTCOL, buff=0.1, corner_radius=0.1), num_dashes=30).scale([1, 0.7, 1])
+                nodeText = Text("Edge", font=FONT, color=TEXTCOL, font_size=FSIZE).next_to(nodeSurr, RIGHT, buff=0.1)
                 self.play(Create(nodeSurr), run_time=0.5)
                 self.wait(0.2)
                 self.play(Write(nodeText), run_time=0.5)
-                self.wait(1.5)
+                self.wait(1.3)
                 self.play(Uncreate(nodeSurr), Unwrite(nodeText), run_time=0.5, lag_ratio=0.1)
                 self.wait(0.1)
                 # self.play(edge.animate.set_stroke(color=EDGE_COL), run_time=0.3)
                 # self.wait(1.3)
 
-            self.wait(0.3)
+            self.wait(0.6)
             self.play(Unwrite(pointText), run_time=0.2)
+            self.wait(0.1)
+            self.play(graphVertices[n1].Clear(), graphVertices[n2].Clear(), run_time=0.5)
             self.wait(0.5)
 
-        self.wait(1)
+        degreesGroup = VGroup()
+        for i, v in enumerate(vertices):
+            degreeText = Text(str(degrees[v]), font=FONT, color=TEXTCOL, font_size=DEGREE_FONT_SIZE).next_to(nodes[i], DOWN, buff=0)
+            # self.play(Write(degreeText), run_time=0.2)
+            degreesGroup.add(degreeText)
+            self.wait(0.3)
+        
+        degreesGroup.shift([-0.25, 0, 0])
+        self.play(FadeIn(degreesGroup, lag_ratio=0.1), run_time=0.5)
+
+        nodeSurr = DashedVMobject(SurroundingRectangle(degreesGroup.submobjects[-1], color=TEXTCOL, buff=0.1, corner_radius=0.15))
+        nodeText = Text("Degree", font=FONT, color=TEXTCOL, font_size=EXPLANATORY_FONT_SIZE).next_to(nodeSurr, DOWN, buff=0.08)
+        self.play(Create(nodeSurr), run_time=0.5)
+        self.wait(0.2)
+        self.play(Write(nodeText), run_time=0.5)
+        self.wait(1.5)
+        self.play(Uncreate(nodeSurr), Unwrite(nodeText), run_time=0.5, lag_ratio=0.1)
+
+        self.wait(2)
 
 
 class DirectedGraphs(Scene):
