2840d4f143cb62c11efa1a543f3c0afc915b378b
Paarth Siloiya <paaarthsiloiya@gmail.com>
2025-07-12 18:54:36 +0530

    Linear Search is completely done with code and a lot of awesome features

--- Website/website/views.py
+++ Website/website/views.py
@@ -7,7 +7,23 @@ views = Blueprint('views', __name__)
 def home():
     return render_template('home.html', user=current_user)
 
-@views.route('/content')
-@login_required
+@views.route('/dsa')
+# @login_required
 def content():
-    return render_template('content.html', user=current_user)
+    return render_template('dsa.html', user=current_user)
+
+@views.route('/Data Structures')
+def data_structures():
+    return render_template('data_structures.html', user=current_user)
+
+@views.route('/Algorithms')
+def algorithms():
+    return render_template('algorithms.html', user=current_user)
+
+@views.route('/Searching Algorithms')
+def searching_algorithms():
+    return render_template('searching_algorithms.html', user=current_user)
+
+@views.route('/Linear Search')
+def linear_search():
+    return render_template('linear_search.html', user=current_user)
