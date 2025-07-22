f752492090f0f55938984b35d1e1e835ff71cdfd
Paarth Siloiya <paaarthsiloiya@gmail.com>
2025-07-22 18:38:27 +0530

    Doubly Linked List also completed. (For now, may add operations for DLL later)

--- LinkedList.py
+++ LinkedList.py
@@ -799,3 +799,274 @@ class ListRotation(Scene):
         )
 
         self.wait(1)
+
+
+class DoubleNode(VGroup):
+    def __init__(self, value):
+        super().__init__()
+        self.value = value
+        self.next : DoubleNode = None
+        self.prev : DoubleNode = None
+        self.selfAddress = random.randint(1000, 9999)
+        self.nextAddress = 0 if self.next is None else self.next.selfAddress
+        self.prevAddress = 0 if self.prev is None else self.prev.selfAddress
+
+    def setValues(self):
+        self.selfAddressText = Text(f"0x{self.selfAddress}", font_size=SELF_ADDRESS_FONT_SIZE, color=TEXTCOL, font=FONT)
+        self.selfAddressGroup = VGroup(
+            self.selfAddressText,
+            Rectangle(width=NODE_WIDTH, height=0.6, color=SORTCOL)
+        )
+
+        if self.next is None:
+            self.nextAddressText = Text("NULL", font_size=NEXT_ADDRESS_FONT_SIZE, color=TEXTCOL, font=FONT)
+        else:
+            self.nextAddressText = Text(f"0x{self.nextAddress}", font_size=NEXT_ADDRESS_FONT_SIZE, color=TEXTCOL, font=FONT)
+        self.nextAddressGroup = VGroup(
+            self.nextAddressText,
+            Rectangle(width=NODE_WIDTH, height=0.6, color=SORTCOL)
+        )
+
+        if self.prev is None:
+            self.prevAddressText = Text("NULL", font_size=NEXT_ADDRESS_FONT_SIZE, color=TEXTCOL, font=FONT)
+        else:
+            self.prevAddressText = Text(f"0x{self.prevAddress}", font_size=NEXT_ADDRESS_FONT_SIZE, color=TEXTCOL, font=FONT)
+        self.prevAddressGroup = VGroup(
+            self.prevAddressText,
+            Rectangle(width=NODE_WIDTH, height=0.6, color=SORTCOL)
+        )
+
+        self.valueText = Text(str(self.value), font_size=VALUE_FONT_SIZE, color=TEXTCOL, font=FONT)
+        self.valueTextGroup = VGroup(
+            self.valueText,
+            Rectangle(width=NODE_WIDTH, height=1.6, color=SORTCOL)
+        )
+
+        self.add(
+            self.prevAddressGroup,
+            self.selfAddressGroup,
+            self.valueTextGroup,
+            self.nextAddressGroup,
+        )
+        self.arrange(DOWN, buff=0.0)
+
+    def updateSelfAddress(self, new_address):
+        self.selfAddress = new_address
+        new_self_address_text = Text(f"0x{self.selfAddress}", font_size=SELF_ADDRESS_FONT_SIZE, color=TEXTCOL, font=FONT).move_to(self.selfAddressText)
+        return self.selfAddressText.animate.become(new_self_address_text)
+    
+    def updateNext(self, new_node):
+        self.next : DoubleNode = new_node
+        self.nextAddress = 0 if self.next is None else self.next.selfAddress
+        if self.next is None:
+            new_next_address_text = Text("NULL", font_size=NEXT_ADDRESS_FONT_SIZE, color=TEXTCOL, font=FONT).move_to(self.nextAddressText)
+        else:
+            new_next_address_text = Text(f"0x{self.nextAddress}", font_size=NEXT_ADDRESS_FONT_SIZE, color=TEXTCOL, font=FONT).move_to(self.nextAddressText)
+        return self.nextAddressText.animate.become(new_next_address_text)
+ 
+    def updatePrev(self, new_node):
+        self.prev : DoubleNode = new_node
+        self.prevAddress = 0 if self.prev is None else self.prev.selfAddress
+        if self.prev is None:
+            new_prev_address_text = Text("NULL", font_size=NEXT_ADDRESS_FONT_SIZE, color=TEXTCOL, font=FONT).move_to(self.prevAddressText)
+        else:
+            new_prev_address_text = Text(f"0x{self.prevAddress}", font_size=NEXT_ADDRESS_FONT_SIZE, color=TEXTCOL, font=FONT).move_to(self.prevAddressText)
+        return self.prevAddressText.animate.become(new_prev_address_text)
+    
+
+class DoubleNodeExplanation(Scene):
+    def construct(self):
+        node1 = DoubleNode(4)
+        node2 = DoubleNode(1)
+
+        node1.setValues()
+        node2.setValues()
+
+        self.play(Create(node1))
+        self.wait(0.4)
+
+        surroundingRectangleNode = DashedVMobject(SurroundingRectangle(node1, buff=0.4, corner_radius=0.2, color=SORTCOL), num_dashes=30)
+        self.play(Create(surroundingRectangleNode))
+        explanatoryText = Text("NODE", color=EXPLANATORY_FONT_COLOR, font=FONT, font_size=EXPLANATORY_FONT_SIZE).next_to(surroundingRectangleNode, LEFT).shift(LEFT * 1.2)
+        explanatoryText = VGroup(
+            explanatoryText,
+            Arrow(start=explanatoryText.get_right(), end=surroundingRectangleNode.get_left(), color=SORTCOL, tip_shape=StealthTip, max_stroke_width_to_length_ratio=4, max_tip_length_to_length_ratio=0.2)
+        )
+
+        self.play(Write(explanatoryText), run_time=0.5)
+        self.wait(0.7)
+        self.play(Unwrite(explanatoryText), FadeOut(surroundingRectangleNode), run_time=0.5)
+        self.wait(0.3)
+
+        explanatoryTextGroup = VGroup()
+
+        explanatoryText = Text("Address of\nprevious node", color=EXPLANATORY_FONT_COLOR, font=FONT, font_size=EXPLANATORY_FONT_SIZE).next_to(node1.prevAddressGroup, LEFT).shift(LEFT * 0.7).shift(UP * 0.5)
+        explanatoryText = VGroup(
+            explanatoryText,
+            Arrow(start=explanatoryText.get_right(), end=node1.prevAddressGroup.get_left(), color=SORTCOL, tip_shape=StealthTip, max_stroke_width_to_length_ratio=2, max_tip_length_to_length_ratio=0.1)
+        )
+        self.play(Write(explanatoryText), run_time=0.5)
+        explanatoryTextGroup.add(explanatoryText)
+
+        explanatoryText = Text("Address of\nthis node", color=EXPLANATORY_FONT_COLOR, font=FONT, font_size=EXPLANATORY_FONT_SIZE).next_to(node1.selfAddressGroup, LEFT).shift(LEFT)
+        explanatoryText = VGroup(
+            explanatoryText,
+            Arrow(start=explanatoryText.get_right(), end=node1.selfAddressGroup.get_left(), color=SORTCOL, tip_shape=StealthTip, max_stroke_width_to_length_ratio=2, max_tip_length_to_length_ratio=0.1)
+        )
+        self.play(Write(explanatoryText), run_time=0.5)
+        explanatoryTextGroup.add(explanatoryText)
+
+
+        explanatoryText = Text("Value of\nthis node", color=EXPLANATORY_FONT_COLOR, font=FONT, font_size=EXPLANATORY_FONT_SIZE).next_to(node1.valueTextGroup, LEFT).shift(LEFT)
+        explanatoryText = VGroup(
+            explanatoryText,
+            Arrow(start=explanatoryText.get_right(), end=node1.valueTextGroup.get_left(), color=SORTCOL, tip_shape=StealthTip, max_stroke_width_to_length_ratio=2, max_tip_length_to_length_ratio=0.1)
+        )
+        self.play(Write(explanatoryText), run_time=0.5)
+        explanatoryTextGroup.add(explanatoryText)
+
+
+        explanatoryText = Text("Address of\nnext node", color=EXPLANATORY_FONT_COLOR, font=FONT, font_size=EXPLANATORY_FONT_SIZE).next_to(node1.nextAddressGroup, LEFT).shift(LEFT)
+        explanatoryText = VGroup(
+            explanatoryText,
+            Arrow(start=explanatoryText.get_right(), end=node1.nextAddressGroup.get_left(), color=SORTCOL, tip_shape=StealthTip, max_stroke_width_to_length_ratio=2, max_tip_length_to_length_ratio=0.1)
+        )
+        self.play(Write(explanatoryText), run_time=0.5)
+        explanatoryTextGroup.add(explanatoryText)
+
+        self.wait(1.2)
+
+        self.play(Unwrite(explanatoryTextGroup))
+
+        self.play(node1.animate.shift(LEFT * 1.5))
+
+
+        node2.shift(RIGHT * 1.5)
+        self.play(Create(node2))
+
+
+        start = node1.nextAddressGroup.get_right()
+        end = node2.selfAddressGroup.get_left() + (LEFT * 0.1) 
+        arrowCurve = CubicBezier(start, start + RIGHT, end + LEFT, end, color=SORTCOL)
+        arrowhead = StealthTip(fill_opacity=1, stroke_opacity=1, color=SORTCOL).scale(0.8)
+        arrowhead.move_to(end)
+        arrowhead.rotate(angle_of_vector(RIGHT))
+
+        arrow = VGroup(arrowCurve, arrowhead)
+        self.play(Create(arrow))
+        self.play(node1.updateNext(node2))
+
+        self.wait(1)
+
+        start = node2.prevAddressGroup.get_left()
+        end = node1.selfAddressGroup.get_right() + (RIGHT * 0.1)
+        arrowCurve = CubicBezier(start, start + LEFT, end + RIGHT, end, color=SORTCOL)
+        arrowhead = StealthTip(fill_opacity=1, stroke_opacity=1, color=SORTCOL).scale(0.8)
+        arrowhead.move_to(end)
+        arrowhead.rotate(angle_of_vector(LEFT))
+
+        arrow = VGroup(arrowCurve, arrowhead)
+        self.play(Create(arrow))
+        self.play(node2.updatePrev(node1))
+
+        self.wait(1)
+
+
+class CreateDoublyLinkedList(Scene):
+    def construct(self):
+        def make_dynamic_bezier_updater_front(start_mobj, end_mobj, offset_start=RIGHT*0.2, offset_end=LEFT*0.1):
+            def updater(curve):
+                start = start_mobj.get_right() + offset_start
+                end = end_mobj.get_left() + offset_end
+                control1 = start + RIGHT
+                control2 = end + LEFT
+                curve.become(CubicBezier(start, control1, control2, end, color=curve.color))
+            return updater
+        
+        def make_dynamic_bezier_updater_back(start_mobj, end_mobj, offset_start=RIGHT*0.2, offset_end=LEFT*0.1):
+            def updater(curve):
+                start = start_mobj.get_left() + offset_start
+                end = end_mobj.get_right() + offset_end
+                control1 = start + LEFT
+                control2 = end + RIGHT
+                curve.become(CubicBezier(start, control1, control2, end, color=curve.color))
+            return updater
+
+        def make_arrowhead_updater(bezier_curve, offset=0.01):
+            def updater(arrowhead):
+                end_point = bezier_curve.point_from_proportion(1)
+                direction = bezier_curve.point_from_proportion(1) - bezier_curve.point_from_proportion(1 - offset)
+                arrowhead.move_to(end_point)
+                arrowhead.set_angle(angle_of_vector(direction))
+            return updater
+        
+        values = [5, 2, 4, 6, 3, 1]
+        # nodes : list[DoubleNode] = [DoubleNode(data) for data in values]
+
+        
+        # for i in range(1, len(values)):
+        #     nodes[i].prev = nodes[i - 1]
+        #     nodes[i - 1].next = nodes[i]
+
+        # head = nodes[0]
+
+        # for node in nodes:
+        #     node.setValues()
+
+
+        head = DoubleNode(values[0])
+        head.setValues()
+        current = head
+
+        nodeGroup = VGroup(head)
+        nodeGroup.arrange(RIGHT, buff=1)
+        self.play(FadeIn(nodeGroup))
+
+        forwardArrows = VGroup()
+        backwardArrows = VGroup()
+
+        for data in values[1:]:
+            node = DoubleNode(data)
+            current.next = node
+            node.prev = current
+            node.setValues()
+            node.next_to(nodeGroup, RIGHT, buff=1)
+
+            self.play(Create(node))
+            nodeGroup.add(node)
+            self.play(nodeGroup.animate.arrange(RIGHT, buff=1))
+
+            if current.next is not None:
+                start = current.nextAddressGroup.get_right()
+                end = current.next.selfAddressGroup.get_left() + (LEFT * 0.1)
+                arrowCurve = CubicBezier(start, start + RIGHT, end + LEFT, end, color=SORTCOL)
+                arrowCurve.add_updater(make_dynamic_bezier_updater_front(current.nextAddressGroup, current.next.selfAddressGroup, 0, LEFT * 0.1))
+                arrowhead = StealthTip(fill_opacity=1, stroke_opacity=1, color=SORTCOL).scale(0.8)
+                arrowhead.move_to(end)
+                arrowhead.rotate(angle_of_vector(RIGHT))
+                arrowhead.add_updater(make_arrowhead_updater(arrowCurve))
+
+                arrow = VGroup(arrowCurve, arrowhead)
+                forwardArrows.add(arrow)
+                self.play(Create(arrow), run_time=0.6)
+                self.play(current.updateNext(current.next), run_time=0.1)
+
+            if node.prev is not None:
+                start = node.prevAddressGroup.get_left()
+                end = node.prev.selfAddressGroup.get_right() + (RIGHT * 0.1)
+                arrowCurve = CubicBezier(start, start + LEFT, end + RIGHT, end, color=SORTCOL)
+                arrowCurve.add_updater(make_dynamic_bezier_updater_back(node.prevAddressGroup, node.prev.selfAddressGroup, 0, RIGHT * 0.1))
+                arrowhead = StealthTip(fill_opacity=1, stroke_opacity=1, color=SORTCOL).scale(0.8)
+                arrowhead.move_to(end)
+                arrowhead.rotate(angle_of_vector(LEFT))
+                arrowhead.add_updater(make_arrowhead_updater(arrowCurve))
+
+                arrow = VGroup(arrowCurve, arrowhead)
+                backwardArrows.add(arrow)
+                self.play(Create(arrow), run_time=0.6)
+                self.play(node.updatePrev(node.prev), run_time=0.1)
+
+            current = node
+
+        self.wait(1)
+        
